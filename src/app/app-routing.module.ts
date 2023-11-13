68f7bedd4e2715251bb900923ca86aba14f61403
wanfadger <galiwango.fahad@gmail.com>
2023-11-13 22:49:06 +0300

    - added view timetable route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TimetableComponent } from './timetable/timetable.component';
 import { AuthenticationGuard } from './authentication/authentication.guard';
+import { ViewTimetableComponent } from './timetable/view-timetable/view-timetable.component';
 
 const routes: Routes = [
   {path:"" , loadChildren:() => import("./authentication/authentication.module").then((m) => m.AuthenticationModule)},
-  {path:"Timetable" , component:TimetableComponent , canActivate:[AuthenticationGuard]}
+  {path:"Timetable" , component:TimetableComponent , canActivate:[AuthenticationGuard]},
+  {path:"ViewTimetable" , component:ViewTimetableComponent , canActivate:[AuthenticationGuard]}
 ];
 
 @NgModule({
