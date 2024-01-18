a297d75774ee61915f4e22e2d5c5c884b5741286
wanfadger <galiwango.fahad@gmail.com>
2024-01-18 11:36:10 +0300

    updated base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  BASE_URL : 'http://localhost:1403',
+  // BASE_URL : 'http://localhost:1403',
+  BASE_URL : "https://webapi.telaschool.org",
   LOGGED_USER_PROFILE:"uewyioj2wodcn-ew",
   Auth_TOKEN_KEY:"wertedscr-2ewdfvrf"
 };
